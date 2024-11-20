e9c058110e874040054e46afb4dbf814e878fb78
Harold Gao <harold.gao205@gmail.com>
2024-11-20 21:59:22 +1100

    added test case template

--- src/test/JSONSerialisableTest.java
+++ src/test/JSONSerialisableTest.java
@@ -1,10 +1,15 @@
+import com.friska.JSONSerialisable;
 import org.junit.Test;
 
 public class JSONSerialisableTest {
 
     @Test
     public void testTrivial(){
-        
+
+        class EmptyClass implements JSONSerialisable {}
+
+        //TODO
+
     }
 
 }
