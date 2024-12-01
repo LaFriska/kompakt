896c4baf34e1ab168c8f61a0f688083fa8d4c7e4
LaFriska <harold.gao205@gmail.com>
2024-12-01 16:15:44 +1100

    Added javadoc to annotations

--- src/main/java/com/friska/kompakt/annotations/SerialiseAsString.java
+++ src/main/java/com/friska/kompakt/annotations/SerialiseAsString.java
@@ -1,11 +1,18 @@
 package com.friska.kompakt.annotations;
 
+import com.friska.kompakt.JSONSerialisable;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to label a field variable of a class implementing {@link JSONSerialisable} as one that should
+ * be serialised as a string. All fields labeled with this annotation will be serialised as a string type in the resulting
+ * JSON, by calling the {@link Object#toString()} method.
+ * @see JSONSerialisable
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface SerialiseAsString {
-}
+public @interface SerialiseAsString {}
